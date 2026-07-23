88303b9f3e41a9cb304f67608e535e5347b63e82
Muhammad Ahmad <ahmadg310810@gmail.com>
2026-07-23 16:17:07 +0500

    Add files via upload

---
+++ days of week using switch.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+string dayofweek(int daynum){
+	string dayname;
+	switch(daynum){
+		case 0:
+			dayname = "Sunday";
+			break;
+		case 1:
+			dayname = "Monday";
+			break;
+		case 2:
+			dayname = "Tuesday";
+			break;
+		case 3:
+			dayname = "Wednesday";
+			break;
+		case 4:
+			dayname = "Thursday";
+			break;
+		case 5:
+			dayname = "Friday";
+			break;
+		case 6:
+			dayname = "Saturday";
+			break;
+		default:
+			dayname = "Invalid Day Number";
+	}
+	return dayname;
+}
+int main(){
+	cout << dayofweek(3);
+}
